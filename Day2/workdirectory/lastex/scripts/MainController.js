993b15993b5ead2477743129f72e2268f37d019b
alexandrasoare <ada_soare@yahoo.com>
2017-06-28 17:54:15 +0300

    day 2

---
+++ Day2/workdirectory/lastex/scripts/MainController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Alexandra.Soare on 6/28/2017.
+ */
+angular.module('hrApp').controller('MainController',['$rootScope', '$scope', function ($rootScope, $scope) {
+    $scope.EmpList =[
+
+        {
+            label: "List Employees",
+            url: ""
+        }
+
+    ]
+}]);
---
+++ Day2/workdirectory/lastex/scripts/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Alexandra.Soare on 6/28/2017.
+ */
+angular.module('hrApp', []);
